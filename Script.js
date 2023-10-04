1107cc732b3d13318415fd9853a46bfd8e6acf7f
Manjusuddula <142823238+Manjusuddula@users.noreply.github.com>
2023-10-04 19:23:22 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,42 +1,78 @@
-// Sample project data
-const project = {
-    name: "Sample Project",
-    tasks: [],
-};
+* {
+    padding: 0px;
+    margin: 0px;
+}
 
-// Function to add a task to the list
-function addTask() {
-    const taskInput = document.getElementById("task-input");
-    const taskText = taskInput.value.trim();
-    if (taskText) {
-        project.tasks.push(taskText);
-        taskInput.value = "";
-        displayTasks();
+.blogs {
+    width: 100%;
+    min-height: 100vh;
+    background: linear-gradient(125deg, #bc3636, #a73b8c);
+}
+
+.heading h1 {
+    text-align: center;
+    color: black;
+    font-family: Arial, Helvetica, sans-serif;
+    border-bottom: 2px solid #c09f9f;
+    padding: 15px;
+}
+
+.heading h1:hover {
+    transform: translate(100px);
+    color: rgb(25, 21, 16);
+}
+
+.form {
+
+    color: white;
+    font-size: large;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    margin-left: 42%;
+    margin-top: 30px;
+}
+
+input {
+    padding: 5px;
+}
+
+textarea {
+    padding: 50px;
+}
+
+button {
+    padding: 15px;
+    font-size: 17px;
+    width: 120px;
+    background-color: #3038b9;
+    color: #fcfcfc;
+    margin: 20px 0px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: rgb(32, 138, 102);
+}
+
+#form-submission h1 {
+    color: #153677;
+}
+
+#form-submission {
+    display: none;
+    margin-top: 50px;
+    margin-left: 300px;
+}
+
+#title-show,
+#content-show {
+    margin-bottom: 10px;
+    white-space: pre-line;
+}
+
+#image-show,
+#video-show {
+    display: block;
+    margin-top: 10px;
+    max-width: 100%;
+    height: auto;
     }
-}
-
-// Function to display tasks
-function displayTasks() {
-    const taskList = document.getElementById("task-list");
-    taskList.innerHTML = "";
-    project.tasks.forEach((task, index) => {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `${task} <button onclick="removeTask(${index})">Remove</button>`;
-        taskList.appendChild(listItem);
-    });
-}
-
-// Function to remove a task
-function removeTask(index) {
-    project.tasks.splice(index, 1);
-    displayTasks();
-}
-
-// Initialize the project details
-document.getElementById("project-name").textContent = project.name;
-
-// Event listeners
-document.getElementById("add-task-btn").addEventListener("click", addTask);
-
-// Initial display of tasks
-displayTasks();
