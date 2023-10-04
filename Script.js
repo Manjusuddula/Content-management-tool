0eb292a3165e46098fa111099784699e527251e8
Manjusuddula <142823238+Manjusuddula@users.noreply.github.com>
2023-10-04 19:27:36 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,78 +1,23 @@
-* {
-    padding: 0px;
-    margin: 0px;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const blog = document.getElementById('blogform');
+    const titleshow = document.getElementById('title-show');
+    const contentshow = document.getElementById('content-show');
+    const imageshow = document.getElementById('image-show');
+    const videoshow = document.getElementById('video-show');
+    blog.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-.blogs {
-    width: 100%;
-    min-height: 100vh;
-    background: linear-gradient(125deg, #bc3636, #a73b8c);
-}
+        const title = document.getElementById('title').value;
+        const content = document.getElementById('content').value;
+        const image = document.getElementById('image').files[0];
+        const video = document.getElementById('video').files[0];
 
-.heading h1 {
-    text-align: center;
-    color: black;
-    font-family: Arial, Helvetica, sans-serif;
-    border-bottom: 2px solid #c09f9f;
-    padding: 15px;
-}
+        titleshow.textContent = title;
+        contentshow.textContent = content;
+        imageshow.src = URL.createObjectURL(image);
+        videoshow.src = URL.createObjectURL(video);
 
-.heading h1:hover {
-    transform: translate(100px);
-    color: rgb(25, 21, 16);
-}
-
-.form {
-
-    color: white;
-    font-size: large;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    margin-left: 42%;
-    margin-top: 30px;
-}
-
-input {
-    padding: 5px;
-}
-
-textarea {
-    padding: 50px;
-}
-
-button {
-    padding: 15px;
-    font-size: 17px;
-    width: 120px;
-    background-color: #3038b9;
-    color: #fcfcfc;
-    margin: 20px 0px;
-    cursor: pointer;
-}
-
-button:hover {
-    background-color: rgb(32, 138, 102);
-}
-
-#form-submission h1 {
-    color: #153677;
-}
-
-#form-submission {
-    display: none;
-    margin-top: 50px;
-    margin-left: 300px;
-}
-
-#title-show,
-#content-show {
-    margin-bottom: 10px;
-    white-space: pre-line;
-}
-
-#image-show,
-#video-show {
-    display: block;
-    margin-top: 10px;
-    max-width: 100%;
-    height: auto;
-    }
+        document.getElementById('form-submission').style.display = 'block';
+        blog.reset();
+    });
+});
